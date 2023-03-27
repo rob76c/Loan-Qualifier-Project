f17979afe7cda955cf961ceaf2130bba9189a95b
rob76c <rob76c@gmail.com>
2023-03-27 18:50:16 -0400

    Full app change

---
+++ Project Qualifier/loan_qualifier_app/qualifier/utils/fileio.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""Helper functions to load and save CSV data.
+
+This contains a helper function for loading and saving CSV files.
+
+"""
+import csv
+
+
+def load_csv(csvpath):
+    """Reads the CSV file from path provided.
+
+    Args:
+        csvpath (Path): The csv file path.
+
+    Returns:
+        A list of lists that contains the rows of data from the CSV file.
+
+    """
+    with open(csvpath, "r") as csvfile:
+        data = []
+        csvreader = csv.reader(csvfile, delimiter=",")
+
+        # Skip the CSV Header
+        next(csvreader)
+
+        # Read the CSV data
+        for row in csvreader:
+            data.append(row)
+    return data
+
+def save_csv(csvpath, qualifying_loans):
+    #Saves the qualifying loans to a CSV file.
+    with open(csvpath, "w", newline='') as csvfile:
+              csvwriter=csv.writer(csvfile)
+              #create a header
+              header=[""]
+              csvwriter.writerow(header)
+              for row in qualifying_loans:
+                csvwriter.writerow(row)
